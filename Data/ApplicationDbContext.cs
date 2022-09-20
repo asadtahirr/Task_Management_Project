316a26bbe3733c7134a5643f9ae2e0d6186031ef
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-09-20 02:24:35 -0300

    Modified model name for task & updated DbContext.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using project_management_system.Models;
 
 namespace project_management_system.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ProjectTask> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
     }

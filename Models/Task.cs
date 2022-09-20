95cd912cde68bfec2a99a5062e9b30102306b037
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-09-20 02:18:39 -0300

    Added task and user model.

---
+++ Models/Task.cs
@@ -0,0 +1,29 @@
+﻿namespace project_management_system.Models
+{
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Priority { get; set; }
+        public decimal RequiredHours { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual Project Project { get; set; }
+        public string AssignedDeveloperId { get; set; }
+        public virtual User AssignedDeveloper { get; set; }
+        public virtual List<User> Watchers { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public string CreatedById { get; set; }
+        public virtual User CreatedBy { get; set; }
+
+        public Task()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.Now;
+            Watchers = new List<User>();
+            Comments = new List<Comment>();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿namespace project_management_system.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public virtual List<Project> AssignedProjects { get; set; }
+        public virtual List<Task> AssignedTasks { get; set; }
+        public virtual List<Project> CreatedProjects { get; set; }
+        public virtual List<Task> CreatedTasks { get; set; }
+        public virtual List<Task> WatchedTasks { get; set; }
+        public virtual List<Comment> CreatedComments { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.Now;
+
+            AssignedProjects = new List<Project>();
+            AssignedTasks = new List<Task>();
+            CreatedProjects = new List<Project>();
+            CreatedTasks = new List<Task>();
+            WatchedTasks = new List<Task>();
+            CreatedComments = new List<Comment>();
+        }
+    }
+}

382d7697fc3b1b334c86f0ab3cbc6539f842aa0d
asad <asadtahir9999@gmail.com>
2022-09-21 15:33:20 -0500

    Applied Lin Li's solution to fix multiple cascade path issue in vain.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,10 +6,10 @@ namespace project_management_system.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        //public DbSet<ProjectTask> ProjectTasks { get; set; }
-        //public DbSet<User> Users { get; set; }
-        //public DbSet<Project> Projects { get; set; }
-        //public DbSet<Comment> Comments { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}

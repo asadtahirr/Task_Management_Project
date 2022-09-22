246b2ad7e3926f4e11e932cc643f2c1c5bbf937b
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-09-22 15:11:14 -0300

    Added users index GET action.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project_management_system.Data;
+using project_management_system.Models;
+using project_management_system.Models.Users.ViewModels;
+using System.Collections.Generic;
+
+namespace project_management_system.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManager<User> UserManager;
+        private ApplicationDbContext DbContext;
+
+        public UsersController(UserManager<User> userManager, ApplicationDbContext dbContext)
+        {
+            UserManager = userManager;
+            DbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<UserDetailsViewModel> users = await UserManager
+                                        .Users
+                                        .OrderBy(e => e.CreatedAt)
+                                        .Select(
+                                            e => new UserDetailsViewModel()
+                                            {
+                                                Id = e.Id,
+                                                FirstName = e.FirstName,
+                                                LastName = e.LastName,
+                                                Username = e.UserName,
+                                                Email = e.Email
+                                            }
+                                        )
+                                        .ToListAsync();
+
+            IndexViewModel viewModel = new IndexViewModel()
+            {
+                Users = users
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Models/Users/ViewModels/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace project_management_system.Models.Users.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<UserDetailsViewModel> Users { get; set; }
+    }
+}

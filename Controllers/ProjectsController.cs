bfc5108a3991bf7495796ccd24ba9d5617702bcb
asad <asadtahir9999@gmail.com>
2022-09-22 12:20:45 -0500

    Added get action to show all projects

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project_management_system.Data;
+using project_management_system.Models;
+using project_management_system.Models.Projects.ViewModels;
+
+namespace project_management_system.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public ApplicationDbContext DbContext { get; set; }
+
+        public ProjectsController(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;  
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel viewModel = new IndexViewModel();
+
+            viewModel.Projects = await DbContext.Projects
+                .Select(p => new ProjectDetailsViewModel()
+            {
+                Id = p.Id,
+                Title = p.Title,
+                CreatedById = p.CreatedById,
+                CreatedByName = p.CreatedBy.UserName,
+                NumberOfProjectTasks = p.ProjectTasks.Count,
+                ProjectTasks = p.ProjectTasks
+
+            }).ToListAsync();
+          
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/Projects/ViewModels/ProjectDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace project_management_system.Models.Projects.ViewModels
+{
+    public class ProjectDetailsViewModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string CreatedByName { get; set; }
+        public string CreatedById { get; set; }
+        public int NumberOfProjectTasks { get; set; }
+        public List<ProjectTask> ProjectTasks { get; set; }
+    }
+}

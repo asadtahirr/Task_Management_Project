04e6b6d6aa38fed4e39774ddece15f156bfae671
asad <asadtahir9999@gmail.com>
2022-09-22 12:20:45 -0500

    Added get action to show all projects

---
+++ Models/Projects/ViewModels/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace project_management_system.Models.Projects.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<ProjectDetailsViewModel> Projects { get; set; } 
+    }
+}

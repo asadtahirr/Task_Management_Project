1857dca03675b9107de12d40fc418070e89ccb90
LengNan777 <L2452520160@gmail.com>
2022-09-24 18:52:44 -0500

    add index view with pagniation and sort order

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -16,23 +16,60 @@ namespace project_management_system.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, int? pageNumber)
         {
             IndexViewModel viewModel = new IndexViewModel();
 
             viewModel.Projects = await DbContext.Projects
                 .Select(p => new ProjectDetailsViewModel()
-            {
-                Id = p.Id,
-                Title = p.Title,
-                CreatedById = p.CreatedById,
-                CreatedByName = p.CreatedBy.UserName,
-                NumberOfProjectTasks = p.ProjectTasks.Count,
-                ProjectTasks = p.ProjectTasks
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    CreatedById = p.CreatedById,
+                    CreatedByName = p.CreatedBy.UserName,
+                    NumberOfProjectTasks = p.ProjectTasks.Count,
+                    ProjectTasks = p.ProjectTasks
 
-            }).ToListAsync();
-          
-            return View(viewModel);
+                }).ToListAsync();
+
+            @ViewData["CurrentSort"] = sortOrder;
+            ViewData["RequiredHoursSort"] = String.IsNullOrEmpty(sortOrder) ? "RequiredHours" : "";
+            ViewData["PrioritySort"] = sortOrder == "Priority" ? "PriorityDesc" : "Priority";
+            /*var projects = from p in DbContext.Projects
+                            select p;*/
+
+            switch (sortOrder)
+            {
+                case "RequiredHours":
+                    foreach (var project in viewModel.Projects)
+                    {
+                        project.ProjectTasks = project.ProjectTasks.OrderBy(p => p.RequiredHours).ToList();
+                    }
+                    break;
+                case "PriorityDesc":
+                    foreach (var project in viewModel.Projects)
+                    {
+                        project.ProjectTasks = project.ProjectTasks.OrderByDescending(p => p.Priority).ToList();
+                    }
+                    break;
+                case "Priority":
+                    foreach (var project in viewModel.Projects)
+                    {
+                        project.ProjectTasks = project.ProjectTasks.OrderBy(p => p.Priority).ToList();
+                    }
+                    break;
+                default:
+                    foreach (var project in viewModel.Projects)
+                    {
+                        project.ProjectTasks = project.ProjectTasks.OrderByDescending(p => p.RequiredHours).ToList();
+                    }
+                    break;
+            }
+            int pageSize = 10;
+            return DbContext.Projects != null ?
+                          View(await PaginatedList<ProjectDetailsViewModel>.CreateAsync(viewModel.Projects.AsQueryable(), pageNumber ?? 1, pageSize)) :
+                          Problem("Entity set 'ApplicationDbContext.Projects'  is null.");
+            //return View(viewModel.Projects);
         }
     }
 }

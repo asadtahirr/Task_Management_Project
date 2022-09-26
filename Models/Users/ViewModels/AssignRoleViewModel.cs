841e8e877cc7bc89b642278498a44065d65aa550
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-09-25 19:36:12 -0500

    Added AssignRole GET action and refactored code.

---
+++ Models/Users/ViewModels/AssignRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace project_management_system.Models.Users.ViewModels
+{
+    public class AssignRoleViewModel
+    {
+        public List<IdentityRole> Roles { get; set; }
+        public UserDetailsViewModel User { get; set; }
+    }
+}
